054934034e34610f60096daf8f09a9490b03d9a7
Cyber Square <70192724+cybersquare@users.noreply.github.com>
2022-12-21 12:59:10 +0530

    Add files via upload

---
+++ Lab activities/Grade 7/Module 8/Module 8-Q1.py
@@ -0,0 +1,7 @@
+#Step #1: After successfully installing Jupyter write ‘jupyter notebook’ in the terminal/command prompt. This will open a new notebook server on your web browser.
+#Step #2: On the top left corner, click on the new button and select python3. This will open a new notebook tab in your browser where you can start to write your first code.
+#Step #3: Press Enter or click on the first cell in your notebook to go into the edit mode.
+#Step #4: Now you are free to write any code.
+#Step #5: You can run your code by pressing Shift + Enter or the run button provided at the top. An example code is given below:
+
+print("I am an Artificial Intelligence programmer")
